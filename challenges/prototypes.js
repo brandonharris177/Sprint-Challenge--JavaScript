a4f3ea0fbbf6b6d8eddee7ed076edc64ae112a22
Brandon-Harris <brandonharris177@gmail.com>
2020-01-08 11:12:37 -0700

    minor changes to prototype

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -18,7 +18,7 @@ function CuboidMaker(attributes) {
   Formula for cuboid volume: length * width * height
 */
 
-CuboidMaker.prototype.volume = function () {
+CuboidMaker.prototype.volume = function() {
   return this.length * this.width * this.height;
 }
 
@@ -29,7 +29,7 @@ CuboidMaker.prototype.volume = function () {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.surfaceArea = function () {
+CuboidMaker.prototype.surfaceArea = function() {
   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
 }
 
